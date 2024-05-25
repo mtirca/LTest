657dad0c68462fbf5a9defb23a6f9f22b276e75c
Mihnea Tirca <Tirca.Fl.Mihnea@student.utcluj.ro>
2024-05-25 23:56:22 +0300

    labeling refactor

--- Assets/Scripts/Model/Label.cs
+++ Assets/Scripts/Model/Label.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -6,12 +7,21 @@ namespace Model
 {
     public class Label
     {
+        public int Id;
         public string Text;
         public Color Color;
         public List<Vector3> Vertices;
+        public static List<int> ExistingIds;
 
         public Label()
         {
+            int newId;
+            do
+            {
+                newId = Random.Range(Int32.MinValue, Int32.MaxValue);
+            } while (!ExistingIds.Contains(newId));
+
+            Id = newId;
             Color = RandomColor();
             Text = Color.ToString();
             Vertices = new List<Vector3>();
