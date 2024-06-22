41741a48df4ebc2ca62d7bfdddd724996b404897
Mihnea Tirca <Tirca.Fl.Mihnea@student.utcluj.ro>
2024-06-22 14:41:05 +0300

    refactor code

--- Assets/Scripts/Player/MouseMovement.cs
+++ Assets/Scripts/Player/MouseMovement.cs
@@ -5,19 +5,10 @@ namespace Player
     [RequireComponent(typeof(Camera))]
     public class MouseMovement : MonoBehaviour
     {
-        #region UI
-
         [SerializeField] private float mouseXSens = 1.8f;
         [SerializeField] private float mouseYSens = 1.8f;
 
-        #endregion
-
         private void Update()
-        {
-            RotateCamera();
-        }
-
-        private void RotateCamera()
         {
             transform.Rotate(-Input.GetAxis("Mouse Y") * mouseYSens, 0.0f, 0.0f);
             transform.Rotate(0.0f, Input.GetAxis("Mouse X") * mouseXSens, 0.0f);
